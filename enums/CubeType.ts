834f7ded89ef8214cd2634025bc2a856a1627a27
Circus-Pygargus <richard.meuret@laposte.net>
2024-07-17 17:24:36 +0200

    add Cubetype enum

---
+++ enums/CubeType.ts
@@ -0,0 +1,11 @@
+// list of existing cube types used in application
+enum CubeType {
+    '2x2' = '2x2',
+    '3x3' = '3x3',
+    '4x4' = '4x4',
+    '5x5' = '5x5',
+    '6x6' = '6x6',
+    '7x7' = '7x7',
+}
+
+export default CubeType;

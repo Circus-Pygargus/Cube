4d6dc3edf716c8479a06b954e427d22fc4385804
Circus-Pygargus <richard.meuret@laposte.net>
2024-07-19 00:48:51 +0200

    fix some little things in databaseManager, I'm really inattentive

--- src/db/databaseManager.ts
+++ src/db/databaseManager.ts
@@ -11,21 +11,20 @@ if (dbName === undefined || dbName === '') {
 }
 
 const connect = async (): Promise<void> => {
-
     const MONGODB_URI: string = `mongodb://127.0.0.1:27017/${dbName}`;
     try {
         await mongoose.connect(MONGODB_URI);
         console.log(`Connexion à la base de données "${dbName}" réussie.`);
     } catch (err) {
-        console.log('Erreur lors de la connexion à MOngoDB : ', err);
+        console.log('Erreur lors de la connexion à MongoDB : ', err);
         process.exit(1); // exit process with failure
     }
 };
 
-const disconnect = async () => {
+const disconnect = async (): Promise<void> => {
     try {
         await mongoose.connection.close();
-        console.log(`Déconnexion de la base de données "${dbName}" réussie.`)
+        console.log(`Déconnexion de la base de données "${dbName}" réussie.`);
     } catch (err) {
         console.error('Erreur pendant la déconnection de MongoDB : ', err);
         process.exit(1);

1f7ff549c464066fe02bf5227c7d1d15f65fcc87
Circus-Pygargus <richard.meuret@laposte.net>
2024-07-19 00:30:50 +0200

    fix : O_o I had put disconnect method inside connect method !! wow

--- src/db/databaseManager.ts
+++ src/db/databaseManager.ts
@@ -1,11 +1,16 @@
-import mongoose, { disconnect } from "mongoose";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+// load environment variables
+dotenv.config();
+
+const dbName = process.env.NODE_ENV === 'test' ? process.env.TEST_DB_NAME : process.env.DB_NAME;
+if (dbName === undefined || dbName === '') {
+    console.error('La connexion à la base de données n\'a pas pu être établie. Vous devez renseigner les clés DB_NAME et TEST_DB_NAME dans le fichier d\'environnement .env !');
+    process.exit(1); // exit process with failure
+}
 
 const connect = async (): Promise<void> => {
-    const dbName = process.env.NODE_ENV === 'test' ? process.env.TEST_DB_NAME : process.env.DB_NAME;
-    if (dbName === undefined || dbName === '') {
-        console.error('La connexion à la base de données n\'a pas pu être établie. Vous devez renseigner les clés DB_NAME et TEST_DB_NAME dans le fichier d\'environnement .env !');
-        process.exit(1); // exit process with failure
-    }
 
     const MONGODB_URI: string = `mongodb://127.0.0.1:27017/${dbName}`;
     try {
@@ -15,16 +20,16 @@ const connect = async (): Promise<void> => {
         console.log('Erreur lors de la connexion à MOngoDB : ', err);
         process.exit(1); // exit process with failure
     }
+};
 
-    const disconnect = async () => {
-        try {
-            await mongoose.disconnect();
-            console.log(`Déconnexion de la base de données "${dbName}" réussie.`)
-        } catch (err) {
-            console.error('Erreur pendant la déconnection de MongoDB : ', err);
-            process.exit(1);
-        }
-    };
+const disconnect = async () => {
+    try {
+        await mongoose.connection.close();
+        console.log(`Déconnexion de la base de données "${dbName}" réussie.`)
+    } catch (err) {
+        console.error('Erreur pendant la déconnection de MongoDB : ', err);
+        process.exit(1);
+    }
 };
 
 export default { connect, disconnect };

5b41f8b472f1a486bca592f919e345bf701f1abf
Circus-Pygargus <richard.meuret@laposte.net>
2024-07-20 04:44:23 +0200

    add some tests on User creation

--- tests/models/user.test.ts
+++ tests/models/user.test.ts
@@ -1,6 +1,7 @@
 import bcrypt from 'bcrypt';
 import User from "../../src/models/User";
 import dbManager from '../../src/db/databaseManager';
+import { MongoServerError } from 'mongodb';
 
 beforeAll(async () => {
     await dbManager.connect();
@@ -10,13 +11,13 @@ afterAll(async () => {
     await dbManager.disconnect();
 });
 
-// Supprimer tous les utilisateurs après chaque test
-afterEach(async () => {
+// Delete all recorded users before each test
+beforeEach(async () => {
     await User.deleteMany({});
 });
 
 describe('User model', () => {
-    it('should create a user with valid data', async () => {
+    it('should create and save a user with valid data', async () => {
         const user = new User({
             name: 'testuser',
             email: 'testuser@exemple.com',
@@ -27,37 +28,214 @@ describe('User model', () => {
         expect(savedUser._id).toBeDefined();
         expect(savedUser.name).toBe('testuser');
         expect(savedUser.email).toBe('testuser@exemple.com');
+        expect(savedUser.password).toBeDefined();
     });
 
-    it ('should fail to create a user with invalid email', async () => {
-        const user = new User({
-            name: 'testuser2',
-            email: 'invalidemail',
-            password: 'password123'
-        });
-
-        let err: Error | undefined;
-        try {
+    describe('Name property', () => {
+        it('should not create a user with an already recorded name', async () => {
+            const user = new User({
+                name: 'testuser',
+                email: 'testuser@exemple.com',
+                password: 'password123'
+            });
             await user.save();
-        } catch (error: any) {
-            err = error as Error;
-        }
 
-        // here I used ! to indicate to typescript that err and its properties are not null or undefined (and so I don't have any more warnings in my IDE)
-        expect(err!).toBeDefined();
-        expect((err! as any).errors.email).toBeDefined();
-        expect((err! as any).errors.email.message).toBe('Veuillez fournir une adresse email valide');
-    });
+            const user2 = new User({
+                name: 'testuser',
+                email: 'testuser2@exemple.com',
+                password: 'password123'
+            });
+            let err: MongoServerError | undefined;
+            try {
+                await user2.save();
+            } catch (error) {
+                err = error as MongoServerError;
+            }
 
-    it ('should hash the password before saving', async () => {
-        const user = new User({
-            name: 'testuser3',
-            email: 'testuser3@exemple.com',
-            password: 'password123'
+            expect(err).toBeDefined();
+            expect(err?.name).toBe('MongoServerError');
+            expect(err?.code).toBe(11000); // Code d'erreur pour duplication d'index
         });
 
-        const savedUser = await user.save();
-        expect(savedUser.password).not.toBe('password123');
-        expect(await bcrypt.compare('password123', savedUser.password)).toBe(true);
+        it('should not create a user with an empty name', async () => {
+            const user = new User({
+                name: '',
+                email: 'emptyName@example.com',
+                password: 'password123'
+            });
+
+            let err: any;
+            try {
+                await user.save();
+            } catch (error) {
+                err = error;
+            }
+
+            expect(err).toBeDefined();
+            expect(err.name).toBe('ValidationError');
+            expect(err.errors.name).toBeDefined();
+            expect(err.errors.name.message).toBe('Le pseudo est obligatoire !');
+        });
+
+        it('should not create a user with a name shorter than 3', async () => {
+            const user = new User({
+                name: 'ab',
+                email: 'testuser@exemple.com',
+                password: 'password123'
+            });
+
+            let err: any;
+            try {
+                await user.save();
+            } catch (error) {
+                err = error;
+            }
+
+            expect(err).toBeDefined();
+            expect(err.name).toBe('ValidationError');
+            expect(err.errors.name).toBeDefined();
+            expect(err.errors.name.message).toBe('Ce pseudo est trop court. Il doit contenir au moins 3 caractères.');
+        });
+
+        it('should not create a user with a name longer than 25', async () => {
+            const user = new User({
+                name: 'abcdefghijklmnopqrstuvwxyz',
+                email: 'testuser@exemple.com',
+                password: 'password123'
+            });
+
+            let err: any;
+            try {
+                await user.save();
+            } catch (error) {
+                err = error;
+            }
+
+            expect(err).toBeDefined();
+            expect(err.name).toBe('ValidationError');
+            expect(err.errors.name).toBeDefined();
+            expect(err.errors.name.message).toBe('Ce pseudo est trop long. Il doit contenir au maximum 25 caractères.');
+        });
+
+        it('should not create create a user with dangerous name', async () => {
+            const user = new User({
+                name: '{ "$ne": "" }',
+                email: 'testuser@exemple.com',
+                password: 'password123'
+            });
+
+            let err: any;
+            try {
+                await user.save();
+            } catch (error) {
+                err = error;
+            }
+
+            expect(err).toBeDefined();
+            expect(err.name).toBe('ValidationError');
+            expect(err.errors.name).toBeDefined();
+            expect(err.errors.name.message).toBe('Le pseudo ne peut contenir que des lettres, des chiffres, des espaces et des underscores.');
+        });
+    });
+
+    describe('Email property', () => {
+        it('should not create a user with an email already recorded', async () => {
+            const user = new User({
+                name: 'testuser',
+                email: 'testuser@exemple.com',
+                password: 'password123'
+            });
+            await user.save();
+
+            const user2 = new User({
+                name: 'testuser2',
+                email: 'testuser@exemple.com',
+                password: 'password123'
+            });
+            let err: MongoServerError | undefined;
+            try {
+                await user2.save();
+            } catch (error) {
+                err = error as MongoServerError;
+            }
+
+            expect(err).toBeDefined();
+            expect(err?.name).toBe('MongoServerError');
+            expect(err?.code).toBe(11000); // Code d'erreur pour duplication d'index
+        });
+
+        it('should not create create a user with invalid email', async () => {
+            const user = new User({
+                name: 'testuser2',
+                email: 'invalidemail',
+                password: 'password123'
+            });
+
+            let err: any;
+            try {
+                await user.save();
+            } catch (error) {
+                err = error;
+            }
+
+            expect(err).toBeDefined();
+            expect(err.name).toBe('ValidationError');
+            expect(err.errors.email).toBeDefined();
+            expect(err.errors.email.message).toBe('Veuillez fournir une adresse email valide');
+        });
+
+        it('should not create a user with an empty email', async () => {
+            const user = new User({
+                name: 'testuser',
+                email: '',
+                password: 'password123'
+            });
+
+            let err: any;
+            try {
+                await user.save();
+            } catch (error) {
+                err = error;
+            }
+
+            expect(err).toBeDefined();
+            expect(err.name).toBe('ValidationError');
+            expect(err.errors.email).toBeDefined();
+            expect(err.errors.email.message).toBe('L\'email est obligatoire !');
+        });
+    });
+
+    describe('Password property', () => {
+        it('should not create a user with a short password', async () => {
+            const user = new User({
+                name: 'testuser3',
+                email: 'testuser3@exemple.com',
+                password: '123456'
+            });
+
+            let err: any;
+            try {
+                await user.save();
+            } catch (error) {
+                err = error;
+            }
+
+            expect(err).toBeDefined();
+            expect(err.name).toBe('ValidationError');
+            expect(err.errors.password).toBeDefined();
+            expect(err.errors.password.message).toBe('Le mot de passe doit contenir au moins 7 caractères !');
+        });
+
+        it('should hash the password before saving', async () => {
+            const user = new User({
+                name: 'testuser3',
+                email: 'testuser3@exemple.com',
+                password: 'password123'
+            });
+
+            const savedUser = await user.save();
+            expect(savedUser.password).not.toBe('password123');
+            expect(await bcrypt.compare('password123', savedUser.password)).toBe(true);
+        });
     });
 });

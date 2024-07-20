30c4f20e7afdbe22d9d55ec8e4fe860fb1f230ea
Circus-Pygargus <richard.meuret@laposte.net>
2024-07-20 22:59:43 +0200

    remove log of db connection and disconnection for tests; tests results are much easier to read

--- src/db/databaseManager.ts
+++ src/db/databaseManager.ts
@@ -23,7 +23,9 @@ class DatabaseManager {
         try {
             await mongoose.connect(MONGODB_URI);
             this.connection = mongoose.connection;
-            console.log(`Connexion à la base de données "${dbName}" réussie.`);
+            if ( process.env.NODE_ENV !== 'test') {
+                console.log(`Connexion à la base de données "${dbName}" réussie.`);
+            }
         } catch (err) {
             console.log('Erreur lors de la connexion à MongoDB : ', err);
             process.exit(1); // exit process with failure
@@ -36,7 +38,9 @@ class DatabaseManager {
         try {
             await mongoose.connection.close();
             this.connection = null;
-            console.log(`Déconnexion de la base de données réussie.`);
+            if ( process.env.NODE_ENV !== 'test') {
+                console.log(`Déconnexion de la base de données réussie.`);
+            }
         } catch (err) {
             console.error('Erreur pendant la déconnexion de MongoDB : ', err);
             process.exit(1);

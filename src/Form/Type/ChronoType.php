ab6de3475649c61575aead1bf682ec8bab1ce3c1
Circus-Pygargus <richard.meuret@laposte.net>
2025-01-09 13:38:05 +0100

    Add missing void return type in ChronoType::configureOptions()

--- src/Form/Type/ChronoType.php
+++ src/Form/Type/ChronoType.php
@@ -48,7 +48,7 @@ class ChronoType extends AbstractType
             ]);
     }
 
-    public function configureOptions(OptionsResolver $resolver)
+    public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => Chrono::class

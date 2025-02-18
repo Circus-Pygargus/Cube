a5841ebf7c2e14a5d9e5633ada16614269252d09
Circus-Pygargus <richard.meuret@laposte.net>
2025-02-18 23:31:20 +0100

    Add ScrambleMoveService : will be used to get first unresolved scramble in DB or ask for a new one

---
+++ src/Service/ScrambleMoveService.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Service;
+
+use App\CubeType\CubeType;
+use App\Entity\ScrambleMove;
+use App\Repository\ScrambleMoveRepository;
+
+class ScrambleMoveService
+{
+    public function __construct(
+        private ScrambleMoveRepository $scrambleMoveRepository,
+        private ScramblerService $scramblerService,
+    )
+    {
+    }
+
+    public function getNextScrambleMove(
+        CubeType $cubeType,
+    ): ScrambleMove
+    {
+        $unresolvedScramble = $this->scrambleMoveRepository->findFirstUnresolvedByCubeTypeOrNull($cubeType);
+        if ($unresolvedScramble !== Null) {
+            $result = $unresolvedScramble;
+        } else {
+            $result = $this->buildNewScrambleMove($cubeType);
+        }
+
+        return $result;
+    }
+
+    private function buildNewScrambleMove(
+        CubeType $cubeType,
+    ): ScrambleMove
+    {
+        // Generate a new scramble
+        $moves = $this->scramblerService->generateScramble($cubeType);
+
+        // Create scrambleMove
+        $scrambleMove = new ScrambleMove();
+        $scrambleMove->setCubeType($cubeType);
+        $scrambleMove->setMoves($moves);
+
+        return $scrambleMove;
+    }
+}

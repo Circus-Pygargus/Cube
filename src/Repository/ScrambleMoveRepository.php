ecb28a3d46bec723856b8c4b17a3e5b8f4e500bb
Circus-Pygargus <richard.meuret@laposte.net>
2025-02-18 23:28:39 +0100

    Add ScrambleMoveRepository::findFirstUnresolvedByCubeTypeOrNull()

--- src/Repository/ScrambleMoveRepository.php
+++ src/Repository/ScrambleMoveRepository.php
@@ -2,18 +2,58 @@
 
 namespace App\Repository;
 
+use App\CubeType\CubeType;
+use App\Entity\Chrono;
 use App\Entity\ScrambleMove;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\SecurityBundle\Security;
 
 /**
  * @extends ServiceEntityRepository<ScrambleMove>
  */
 class ScrambleMoveRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
+    public function __construct(
+        ManagerRegistry $registry,
+        private Security $security,
+        private LoggerInterface $logger,
+    )
     {
         parent::__construct($registry, ScrambleMove::class);
+    }
+
+    public function findFirstUnresolvedByCubeTypeOrNull(
+        CubeType $cubeType,
+    )
+    {
+        try {
+            $qb = $this->createQueryBuilder('s')
+                ->where('s.cubeType = :cubeType');
+
+            $subQuery = $this->createQueryBuilder('sub')
+                ->select('1')
+                ->from(Chrono::class, 'c')
+                ->where('c.scrambleMove = s.id')
+                ->andWhere('c.user = :user');
+
+            $qb->andWhere(
+                $qb->expr()->not(
+                    $qb->expr()->exists($subQuery->getDQL())
+                )
+            )
+                ->setParameter('cubeType', $cubeType->value)
+                ->setParameter('user', $this->security->getUser())
+                ->orderBy('s.createdAt', 'ASC')
+                ->setMaxResults(1);
+
+            return $qb->getQuery()->getOneOrNullResult();
+        } catch (\Exception $e) {
+            $this->logger->warning('Problème pendant la récupération d\'un mélange en bdd !');
+            $this->logger->error($e->getMessage());
+            return null;
+        }
     }
 
     //    /**

2db3f65a6f17aa06be983a9fb93391a75ab25105
Circus-Pygargus <richard.meuret@laposte.net>
2025-02-21 23:41:34 +0100

    Add submit button in ChronoType

--- src/Form/Type/ChronoType.php
+++ src/Form/Type/ChronoType.php
@@ -10,6 +10,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EnumType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -43,6 +44,9 @@ class ChronoType extends AbstractType
                 ],
             ])
             ->add('scrambleMove', HiddenType::class, [
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Enregistrer',
             ])
             ;
     }

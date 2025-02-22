369291f35a016ad7aafd85b6bfbb5b6777205820
Circus-Pygargus <richard.meuret@laposte.net>
2025-02-22 01:08:29 +0100

    Add : chrono form is only displayed once cube is resolved

--- assets/js/game.js
+++ assets/js/game.js
@@ -217,5 +217,7 @@ document.addEventListener('DOMContentLoaded', () => {
         clearInterval(interval);
         isChronoRunning = false;
         canUseChrono = false;
+        document.querySelector('#chrono-form').classList.remove('hidden');
+        document.querySelector('#clock-container').classList.add('hidden');
     }
 });

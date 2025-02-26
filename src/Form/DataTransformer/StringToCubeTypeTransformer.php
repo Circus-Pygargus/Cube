194af79d63c1c067e7cb8c676c921c23637c5d3d
Circus-Pygargus <richard.meuret@laposte.net>
2025-02-26 11:35:00 +0100

    Add StringToCubeTypeTransformer

---
+++ src/Form/DataTransformer/StringToCubeTypeTransformer.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form\DataTransformer;
+
+use App\CubeType\CubeType;
+use Symfony\Component\Form\DataTransformerInterface;
+use Symfony\Component\Form\Exception\TransformationFailedException;
+
+class StringToCubeTypeTransformer implements DataTransformerInterface
+{
+    public function transform(mixed $cubeType): mixed
+    {
+        if (!$cubeType instanceof CubeType) {
+            return null;
+        }
+
+        return $cubeType->value;
+    }
+
+    public function reverseTransform(mixed $cubeTypeString): mixed
+    {
+        if (!$cubeTypeString || !is_string($cubeTypeString)) {
+            throw new TransformationFailedException('La chaîne cubeType est vide ou invalide.');
+        }
+
+        try {
+            return CubeType::from($cubeTypeString);
+        } catch (\ValueError $e) {
+            throw new TransformationFailedException("'$cubeTypeString' n'est pas une valeur valide pour CubeType.");
+        }
+    }
+}

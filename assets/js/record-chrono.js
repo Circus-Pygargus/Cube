fb6bae018cf7382531e2afa73827eb33cd80d803
Circus-Pygargus <richard.meuret@laposte.net>
2025-08-25 11:05:08 +0200

    Fix : cache le bouton submit de chrono lorsque le chrono est enregistré

--- assets/js/record-chrono.js
+++ assets/js/record-chrono.js
@@ -36,6 +36,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 if (json.isOk === true) {
                     const msgDiv = document.querySelector('#record-msg');
                     msgDiv.innerHTML = json.message;
+                    chronoSubmitBtn.classList.add('hidden');
                 } else {
                     /** @todo afficher un message explicatif à l'utilisateur */
                     console.error('Erreur dans la réponse du serveur:', json.message);
@@ -49,4 +50,5 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 });
+
 

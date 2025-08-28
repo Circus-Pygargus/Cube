188f20bca5532ad6623440de53d0cbd9a3116cfb
Circus-Pygargus <richard.meuret@laposte.net>
2025-08-29 00:10:11 +0200

    Add UserOptions form type

---
+++ src/Form/Type/UserOptionsType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form\Type;
+
+use App\Entity\UserOptions;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserOptionsType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('displaySiteRecords', CheckboxType::class, [
+                'label' => 'Afficher les meilleurs chronos du site',
+                'mapped' => true,
+                'required' => false,
+                'data' => false,
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Enregistrer',
+            ]);
+    }
+    
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => UserOptions::class,
+        ]);
+    }
+}

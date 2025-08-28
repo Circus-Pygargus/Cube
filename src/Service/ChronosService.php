977d95223171d1f148baca95e789c8fc852ebeba
Circus-Pygargus <richard.meuret@laposte.net>
2025-08-29 01:00:07 +0200

    [FEAT] Only display site records in chronosInfos if user asked for it in options page

--- src/Service/ChronosService.php
+++ src/Service/ChronosService.php
@@ -4,11 +4,13 @@ namespace App\Service;
 
 use App\Enum\CubeType;
 use App\Repository\ChronoRepository;
+use Symfony\Bundle\SecurityBundle\Security;
 
 class ChronosService
 {
     public function __construct(
         private ChronoRepository $chronoRepository,
+        private Security $security,
     )
     {        
     }
@@ -17,12 +19,20 @@ class ChronosService
     {
         $result = [];
         $cubeTypes = CubeType::getAsArray();
+        $userOptions = null;
+        if ($this->security->getUser()->getUserOptions()) {
+            $userOptions = $this->security->getUser()->getUserOptions();
+        }
         foreach (CubeType::cases() as $cubeType) {
             $cubeTypeResult = [
                 'cubeType' => $cubeType->value,
-                'siteRecord' => $this->chronoRepository->findSiteRecordByCubeType($cubeType),
                 'personalBest' => $this->chronoRepository->findPersonalBestByCubeType($cubeType)
             ];
+            // Default value for displaying site records is false, so only add them if user has already set option to true
+            if ($userOptions !== null && $userOptions->isDisplaySiteRecords()) {
+                $siteRecords = $this->chronoRepository->findSiteRecordByCubeType($cubeType);
+                $cubeTypeResult['siteRecord'] = $siteRecords;
+            }
             array_push($result, $cubeTypeResult);
         }
 

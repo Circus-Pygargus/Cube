f69f8e992c99044b50f8aefdbc4f97bf7b109b25
Circus-Pygargus <richard.meuret@laposte.net>
2025-08-29 22:01:06 +0200

    Info : add @todos in record-chrono.js

--- assets/js/record-chrono.js
+++ assets/js/record-chrono.js
@@ -18,6 +18,18 @@ document.addEventListener('DOMContentLoaded', () => {
             newChronoDiv.innerHTML = document.querySelector('#readable-duration').innerHTML;
             actualRecordsDiv.appendChild(newChronoDiv);
 
+            /** @Todo Vérifier si le nouveau chrono est meilleur que le siteRecord et le personalBest (début de réfexion ci-dessous) */
+            // let bestRecDiv, worstRecDiv;
+            // console.log(bestRecDiv);
+
+            // Array.from(actualRecordsDiv).sort(function (a, b) { return parseInt(a.getAttribute('data-value'), 10) - parseInt(b.getAttribute('data-value'), 10); })
+            // console.log(actualRecordsDiv);
+            // Array.from(actualRecordsDiv).forEach(recordDiv => {
+            //     // if ()
+            // });
+
+            /** @Todo si plus de 2 chronos de session (même cubeType) ajout css class green au meileur */
+            /** @Todo si plus de 3 chronos de session (même cubeType) ajout css class red au pire */
 
             const chronoForm = document.querySelector('form[name="chrono"]');
             const formData = new FormData(chronoForm);

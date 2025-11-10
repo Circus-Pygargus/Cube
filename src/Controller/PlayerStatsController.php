f2bf20d654249a6a7aaf20758272781e7bb49d83
Circus-Pygargus <richard.meuret@laposte.net>
2025-11-10 16:42:12 +0100

    Refacto : prepare to have several stats route

--- src/Controller/PlayerStatsController.php
+++ src/Controller/PlayerStatsController.php
@@ -2,30 +2,62 @@
 
 namespace App\Controller;
 
+use App\Enum\CubeType;
+use App\Repository\ChronoRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Requirement\EnumRequirement;
 
 class PlayerStatsController extends AbstractController
 {
-    #[Route('/player-stats', name: 'app_player_stat')]
-    #[Route('/player-stats/{year}', name: 'app_player_stat_year', requirements: ['year' => '\d{4}'])]
-    #[Route('/player-stats/{year}/{month}', name: 'app_player_stat_month', requirements: ['year' => '\d{4}', 'month' => '\d{1,2}'])]
-    #[Route('/player-stats/{year}/{month}/{day}', name: 'app_player_stat_day', requirements: ['year' => '\d{4}', 'month' => '\d{1,2}', 'day' => '\d{1,2}'])]
+    #[Route('/user-stats/{cubeType}', name: 'app_user_stats')]
     public function index(
-        int $year = null,
-        int $month = null,
-        int $day = null,
+        ChronoRepository $chronoRepository,
+        CubeType $cubeType = CubeType::CUBE_3X3,
     ):Response
     {
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
-
-        /* Par défaut, afficher les chronos par années,
-            si enregistrement sur une seule année, afficher par mois
-            si enregistrement sur un seul mois, afficher par jours
-        */        
+        
+        $bestsChronos = $chronoRepository->findPersonalBestTimesByYears($cubeType);
 
         return $this->render('player-stats/index.html.twig', [
+            'chronos' => $bestsChronos,
         ]);
     }
+
+    // #[Route('/user-stats/{cubeType}/{year}/{month}', name: 'app_user_stats_month', requirements: ['cubeType' => new EnumRequirement(CubeType::class), 'year' => '\d{4}', 'month' => '\d{1,2}'])]
+    // public function userStatsByMonth(
+    //     ChronoRepository $chronoRepository,
+    //     CubeType $cubeType = CubeType::CUBE_3X3,
+    //     int $year,
+    //     int $month,
+    // ):Response
+    // {
+    //     $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        
+    //     $bestsChronos = $chronoRepository->findPersonalBestTimesByYears($cubeType);
+
+    //     return $this->render('player-stats/index.html.twig', [
+    //         'chronos' => $bestsChronos,
+    //     ]);
+    // }
+    
+    // #[Route('/user-stats/{cubeType}/{year}/{month}/{day}', name: 'app_user_stats_day', requirements: ['cubeType' => new EnumRequirement(CubeType::class), 'year' => '\d{4}', 'month' => '\d{1,2}', 'day' => '\d{1,2}'])]
+    // public function userStatsByDay(
+    //     ChronoRepository $chronoRepository,
+    //     CubeType $cubeType = CubeType::CUBE_3X3,
+    //     int $year,
+    //     int $month,
+    //     int $day,
+    // ):Response
+    // {
+    //     $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        
+    //     $bestsChronos = $chronoRepository->findPersonalBestTimesByYears($cubeType);
+
+    //     return $this->render('player-stats/index.html.twig', [
+    //         'chronos' => $bestsChronos,
+    //     ]);
+    // }
 }
